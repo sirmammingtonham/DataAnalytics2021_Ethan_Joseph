75d8dd88bd0a73a7dd59fc87ea18d1fe912a731c
sirmammingtonham <ethanj217@gmail.com>
2021-04-08 13:19:27 -0700

    end of class push

---
+++ lab7/lab1_svm2.R
@@ -0,0 +1,13 @@
+library(e1071)
+library(rpart)
+data(Ozone, package="mlbench")
+# split data into a train and test set
+index <- 1:nrow(Ozone)
+testindex <- sample(index, trunc(length(index)/3))
+testset <- na.omit(Ozone[testindex,-3])
+trainset <- na.omit(Ozone[-testindex,-3])
+svm.model <- svm(V4 ~ ., data = trainset, cost = 1000, gamma = 0.0001)
+svm.pred <- predict(svm.model, testset[,-3])
+crossprod(svm.pred - testset[,3]) / length(testindex)
+
+summary(svm.model)
